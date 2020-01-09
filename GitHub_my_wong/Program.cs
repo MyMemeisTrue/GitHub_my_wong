1cef0f29acd122ad4ca21f712536da0259f22a63
Måns Danielsson <mans.danielsson@elev.ga.ntig.se>
2020-01-09 13:34:46 +0100

    Add project files.

---
+++ GitHub_my_wong/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHub_my_wong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
